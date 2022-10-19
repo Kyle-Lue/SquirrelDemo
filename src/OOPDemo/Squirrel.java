e6c2ad18698388a682f75ba639c151178aad9941
Kyle-Lue <jeffrey.lue23@ycdsbk12.ca>
2022-10-19 10:40:16 -0400

    Squirrel Demo Complete

---
+++ src/OOPDemo/Squirrel.java
@@ -0,0 +1,46 @@
+package OOPDemo;
+
+public class Squirrel{
+    // instance variables
+    String colourString;
+    String SizeString;
+    int nutCountInt;
+    boolean aliveBoolean;
+    double iqDouble;
+
+    // constructor
+    public Squirrel(String newColourString, String newSizeString, int newInt, int newNutCount, double newIQdouble){
+        this.aliveBoolean = true;
+        this.colourString = newColourString;
+        this.SizeString = newSizeString;
+        this.nutCountInt = newNutCount;
+        this.iqDouble = newIQdouble;
+    }
+
+    // instance methods
+
+    public void run(){
+        System.out.println("run!!");
+        
+    }
+
+    public void findNut(int numberNuts){
+        this.nutCountInt = this.nutCountInt + numberNuts;
+    }
+
+    public int getNutCount(){
+        return this.nutCountInt;
+    }
+
+    public void eatNut(){
+        this.nutCountInt--;
+    }
+
+    public void climb(){
+        System.out.println("Climb that tree!!");
+    }
+
+    public void kill(){
+        this.aliveBoolean = false;
+    }
+}
